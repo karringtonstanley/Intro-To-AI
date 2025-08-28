c409f11fd8b77fda8e985c4ef84cb80dadc3d403
karringtonstanley <143848747+karringtonstanley@users.noreply.github.com>
2025-08-28 14:38:58 -0400

    Delete Simple-Linear-Regression (Autosaved).R

--- Simple-Linear-Regression (Autosaved).R
+++
@@ -1,7 +0,0 @@
-# Author Karrington Stanley Date: Aug 28, 2025; Purpose: Test simple linear regeression
-
-#Get some dummy dataset
-cars <- mtcars
-
-#Scatter plot on training data
-scatter.smooth(x=cars$mpg, y=cars$disp, main="SpeedVSdistance")
